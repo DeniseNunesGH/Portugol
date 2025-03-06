934ed483a8846deeaf9820ce6e838da7a7131f9c
DeniseNunesGH <selineliine@gmail.com>
2025-03-06 11:00:12 -0300

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,48 @@
+fun main() {
+    //fun = function -> main
+    // main
+    //print = escreva
+    println("Bolo de Cenoura")
+
+    //ctrl + shift + f10 também roda o programa
+
+    var idade = 10
+    var nome = "Cage"
+
+    var a = 5
+    a = readln.toInt().toDouble()
+    //toInt = to integrate
+    //toDouble =
+    //leia = READLN = a ideia é sempre guardar um texto para iniciar
+    var b = 10.1
+
+    var c = a + b
+    var d = a - b
+    var e = a * b
+    var f = a / b
+
+    print(a)
+    print(b)
+    print(c)
+
+    //diferença do print pro printIn = o primeiro não pula linha, o segundo sim
+
+    //atalho ctrl + d = escreve todas as línhas dos códigos iguais
+
+    //os símbolos e expressões são as mesmas, porém no kotlin você declara uma variavel e um valor
+    //var = variável é lida tanto quanto real quanto inteiro
+
+    if (a > b){
+        print("A é meior que B")
+    } else {
+        print("Outro resultado")
+    }
+
+    //se e senao = if e else
+
+
+    //W3 SCHOOLS É UMA BIBLIOTECA QUE AUXILIA
+    //OU KOTLINLANG.ORG que é a o site oficial da documentação de kotlin
+
+
+}

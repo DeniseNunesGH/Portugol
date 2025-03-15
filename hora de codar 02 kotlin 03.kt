d1bd5b69f7e0d955d9d20f1b7f46b57f82a10844
DeniseNunesGH <selineliine@gmail.com>
2025-03-15 15:27:15 -0300

    Add files via upload

---
+++ hora de codar 02 kotlin 03.kt
@@ -0,0 +1,17 @@
+fun main() {
+println("Informe um valor")
+    var valor = readln().toInt()
+
+
+    if (valor > 0){
+        println("O $valor é positivo")
+    }
+
+    if (valor > 0){
+        println("O $valor é negativo")
+    }
+
+    if (valor == 0) {
+        println("O $valor é zero")
+    }
+}

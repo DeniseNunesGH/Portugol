4ce7a6354c877e409a2213406dedd34b04b0466e
DeniseNunesGH <selineliine@gmail.com>
2025-03-15 15:21:50 -0300

    Add files via upload

---
+++ hora de codar 02 kotlin 01.kt
@@ -0,0 +1,15 @@
+fun main() {
+    println("Informe o primeiro número: ")
+    var num1 = readln().toInt()
+
+    println("Informe o segundo número: ")
+    var num2 = readln().toInt()
+
+    if (num1 > num2) {
+        println("O 1° é maior")
+    }
+    else {
+        println("O 2° é o maior")
+    }
+
+}

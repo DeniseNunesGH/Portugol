f22f3743af0e070b33168a6066b915e913fe7c67
DeniseNunesGH <selineliine@gmail.com>
2025-03-17 12:49:17 -0300

    Add files via upload

---
+++ Hora de codar 02 kotlin 05.kt
@@ -0,0 +1,26 @@
+fun main () {
+//5.
+
+    println("Informe o primeiro valor: ")
+    var valor1 = readln().toFloat()
+
+    println("Informe o segundo valor: ")
+    var valor2 = readln().toFloat()
+
+    println("Informe o terceiro valor: ")
+    var valor3 = readln().toFloat()
+
+    println("Informe o quarto valor: ")
+    var valor4 = readln().toFloat()
+
+    println("Informe o quinto valor: ")
+    var valor5 = readln().toFloat()
+
+    println("Informe o sexto valor: ")
+    var valor6 = readln().toFloat()
+
+    var media = (valor1 + valor2 + valor3 + valor4 + valor5 + valor6) / 6
+
+    println("\nO valor da média é de: $media")
+
+}

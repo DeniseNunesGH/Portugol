9af59b509eb51e1f5129d4e7f1a06031c3ff14b4
DeniseNunesGH <selineliine@gmail.com>
2025-03-19 23:22:36 -0300

    Add files via upload

---
+++ (rascunho) hora de codar 06 AGENDA kotlin.kt
@@ -0,0 +1,44 @@
+fun main () {
+    //desenvolver uma agenda com:
+    //nomes, endereços, telefones
+    //quant: 10 pessoas
+    //menu de opções
+
+    //a. cadastrar os 10 registros
+    //b. pesquisar um dos 10 registros de cada vez pelo campo nome (usar o metodo sequencial)
+    //c. classificar por ordem de nome os registros cadastrados
+    //d. apresentar todos os registros
+    //e. sair do programa cadastrado
+
+    var contato = mutableListOf<String>()
+    var nome = String
+    var endereco = String
+    var telefones = String
+
+        println(
+            """MENU AGENDA
+        |1. Adicionar contato 
+        |2. Contatos
+        |3. Pesquisar
+        |4. Sair
+    """.trimMargin()
+        )
+        var opcao = readln().toInt()
+
+
+
+
+    }
+
+
+
+
+
+
+    //array = para lista
+//1. when ->  mais prático do que while
+//2. println normal
+//3. puxar alguma funcao para pesquisar em ((contatos))
+//4. break ou pare ou sei lá
+
+
